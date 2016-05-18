601c52273c3b7482c05cf2cc84d1e96aa12568fe
VanityPlate <alexducken@gmail.com>
2016-05-18 15:08:47 -0700

    Alex incoming
    More changes to environment/t_actions

--- environment.py
+++ environment.py
@@ -1,20 +1,14 @@
 import numpy as np
 import tile
+import animal
+from animal import Deer, Wolf, Animal
 
 class environment(object):
 
-    #values for storing the grid of tiles
-    width = 0
-    height = 0
-    grid = None
-
 #################################################################
-#                    Eviromental Constants
+#                    Environment Static Variables
 #################################################################
-    #constants for state of the enviroment as a whole
-    teff_total_mass = 0 #in pounds
-    tree_total_mass = 0 #in pounds
-    total_volume_groundwater = 0 #in gallons
+    #conversions
     square_inches_acres = 6272640
     cubic_inches_to_gallons = 0.004329
 
@@ -34,7 +28,7 @@ class environment(object):
     teff_seed_date = 225 #day of year teff tests to seed
     max_teff_acre = 16000 #pounds
     threshold_teff_acre = 177 #pounds
-    teff_death_chance = .1 #chance teff is wiped out if below threshold
+    teff_death_chance = .01 #chance teff is wiped out if below threshold
 
     #Resevoir / Well Constants
     rez_max_volume = 10000 #in gallons
@@ -51,6 +45,20 @@ class environment(object):
 #                    Constructor
 #################################################################
     def __init__(self, in_width, in_height):
+        #variables for environment
+        teff_total_mass = 0  # in pounds
+        tree_total_mass = 0  # in pounds
+        total_volume_groundwater = 0  # in gallons
+
+        # values for storing the grid of tiles
+        width = 0
+        height = 0
+        grid = None
+
+        #values for controlling animals
+        total_mass_animal = 0 #in lbs
+        animals_alive = {Deer: 0, Wolf: 0}
+
         #setting size of grid and creating grid
         self.height = in_height
         self.width = in_width
@@ -70,3 +78,10 @@ class environment(object):
                     res = True
                 grid[x,i] = tile(res, self.soil_types[1], well, \
                                  self)
+                grid[x,i].tile_x = x
+                grid[x,i].tile_y = i
+                well = False
+                res = False
+
+        def update(day_in):
+            grid[:,:].update
--- tile_actions.py
+++ tile_actions.py
@@ -1,2 +1,16 @@
+import tile
+import environment
 
+#################################################################
+#                    Functions that Tile uses to
+#                   Manage Teff growth and Water Distribution
+#################################################################
 
+def teff_grow(tile_in):
+    return 0
+
+def water_manage(tile_in):
+    return 0
+
+def seed_tiles(tile_in):
+    return 0
